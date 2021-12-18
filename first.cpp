17ec14db8084a7f4402c817ff9eb66ca9cf3d554
Mukesh Jaiswal <56516302+MukeshJaiswal01@users.noreply.github.com>
2021-12-18 08:13:08 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -4,6 +4,10 @@
 #include <math.h>
 #include <omp.h>
 
+double s1, s2, e1, e2, clk1, clk2;
+int n;
+
+       // struct for the restaurant
  struct R_data{
            
             int no_of_customer;
@@ -13,6 +17,7 @@
             int no_of_rava_idli;
        };
        
+       // for total calculation
  struct Total_Data{
             int no_of_customer;
             int no_of_idlis;
@@ -20,7 +25,7 @@
             int no_of_dosas;
             int no_of_rava_idli;
  };
- 
+       // sending specific restaurant data
  struct Receive_data{
      
             int no_of_customer;
@@ -39,7 +44,7 @@ int main()
     struct R_data r_data[8];
     struct Total_Data t_data;
     struct Receive_data R_data[8];
-    int n;
+  
     
     for(int i = 0; i < 8; i++){
         
@@ -50,6 +55,10 @@ int main()
         r_data[i].no_of_rava_idli = rand() % 10;
     }
     
+    printf("Enter the restaurant number\n\n");
+         
+    scanf("%d", &n);
+         
    
    
     
@@ -57,24 +66,27 @@ int main()
     
      {
          printf("first part of the question\n");
-         printf("Enter the restaurand number\n");
+
          
-          scanf("%d", &n);
-         
-         
-             
-          R_data[n-1].no_of_customer = r_data[n-1].no_of_customer;
-          R_data[n-1].no_of_idlis = r_data[n-1].no_of_idlis;
-          R_data[n-1].no_of_vada = r_data[n-1].no_of_vada;
-          R_data[n-1].no_of_dosas = r_data[n-1].no_of_dosas;
-          R_data[n-1].no_of_rava_idli = r_data[n-1].no_of_rava_idli;
+         s1 = clock();   
+          
+          R_data[6].no_of_customer = r_data[6].no_of_customer;
+          R_data[6].no_of_idlis = r_data[6].no_of_idlis;
+          R_data[6].no_of_vada = r_data[6].no_of_vada;
+          R_data[6].no_of_dosas = r_data[6].no_of_dosas;
+          R_data[6].no_of_rava_idli = r_data[6].no_of_rava_idli;
+          
+          e1 = clock();
           
           printf(" restaurant number : M(%d)\n", n-1);
-          printf(" customer %d \n",R_data[n-1].no_of_customer);
-          printf(" idlis %d \n", R_data[n-1].no_of_idlis );
-          printf(" vada %d\n",  R_data[n-1].no_of_vada);
-          printf(" dosa %d\n",  R_data[n-1].no_of_dosas);
-          printf("rava_idli %d\n", R_data[n-1].no_of_rava_idli);
+          printf(" customer %d \n",R_data[6].no_of_customer);
+          printf(" idlis %d \n", R_data[6].no_of_idlis );
+          printf(" vada %d\n",  R_data[6].no_of_vada);
+          printf(" dosa %d\n",  R_data[6].no_of_dosas);
+          printf("rava_idli %d\n\n", R_data[6].no_of_rava_idli);
+          
+          clk1 = double(e1 - s1) / CLOCKS_PER_SEC;
+          printf("time : %lf\n\n", clk1);
           
          
         
@@ -83,7 +95,7 @@ int main()
          
          
          
-         
+        s2 = clock();
          
         for(int i = 0; i<8; i++)
         
@@ -103,6 +115,11 @@ int main()
     printf("total vada %d\n",t_data.no_of_vada );
     printf("total dosa %d\n",t_data.no_of_dosas);
     printf("total rava_idli %d\n",t_data.no_of_rava_idli );
+    
+    e2 = clock();
+    
+    clk2 = double(s2 - e2) / CLOCKS_PER_SEC;
+          printf("time : %lf\n\n", clk2);
     
     
     
